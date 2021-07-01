eb07569c37488ddbda2b8c438f2ef35ff1a70a0f
Gabriel Vasquez Guerra <gabriel.vasquez391@hotmail.com>
2021-07-01 00:33:44 -0500

    fixed db

--- TwoNEL.API/Startup.cs
+++ TwoNEL.API/Startup.cs
@@ -73,7 +73,7 @@ namespace TwoNEL.API
             services.AddDbContext<AppDbContext>(options =>
             {
                 //options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"));
-                options.UseMySQL(Configuration.GetConnectionString("ClearDBMySQLConnection"));
+                options.UseMySQL(Configuration.GetConnectionString("SmarterAspMySQLConnection"));
             });
 
             // Dependency Injection Configuration
